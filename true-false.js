567495d812f2c944fabcd8e6e77ec0d4e11edeab
Khairulbasharrr <khairulbashar9024@gmail.com>
2021-08-24 15:11:23 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// falsy
+// const name = "0" ;
+// 0
+// ""
+// undefined 
+// null 
+// NaN
+// truthy 
+// " "
+// "0"
+
+
+    let name = false ;
+if( name  ){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+
+}
