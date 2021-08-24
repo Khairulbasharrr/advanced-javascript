4a6146960a4b085351d3d6e0b4664bf97be9d5b6
Khairulbasharrr <khairulbashar9024@gmail.com>
2021-08-24 12:01:27 +0600

    some add

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,14 @@
+const first = 0;
+const second = false;
+if (first  === second){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+// triple-equal{
+//     value + type check ;
+// }
+// second equal {
+//     only value check;
+// }
